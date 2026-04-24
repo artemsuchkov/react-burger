30625868e7e4c66a5124e264cc67ff4e4570640f
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2026-04-24 16:16:32 +0300

    add notfound page ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,7 +3,7 @@ import { ForgotPasswordPage } from './forgotpassword/forgot-password.tsx';
 import { HomePage } from './home/home.tsx';
 import { LoginPage } from './login/login-page.tsx';
 import { IngredientsDetails } from './modal/ingredients-details.tsx';
-import { NotFoundPage } from './not-found/not-found.jsx';
+import { NotFoundPage } from './not-found/not-found.tsx';
 import { ProfileOrderPage } from './profile/order/ProfileOrderPage.jsx';
 import { ProfilePage } from './profile/profile-page.jsx';
 import { RegisterPage } from './register/register-page.jsx';
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from 'react';
+
+export const NotFoundPage = (): ReactElement => {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  );
+};
