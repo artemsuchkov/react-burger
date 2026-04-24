f66357f2f440d3c7b000d86fb6529ab63c656d3a
Artem Suchkov Artmin Notebook <artemsuchkov@yandex.ru>
2026-04-24 13:26:58 +0300

    add tokens.ts

--- src/services/user/actions.ts
+++ src/services/user/actions.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { api } from '@/utils/api-user.js';
-import { isTokenExists } from '@utils/tokens.js';
+import { isTokenExists } from '@utils/tokens.ts';
 
 import { setUser, setIsAuthChecked } from './slice.js';
 
